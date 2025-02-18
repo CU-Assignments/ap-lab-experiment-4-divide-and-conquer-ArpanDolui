766ca3e4724177600ca350bcd7242930b0fffd32
ArpanDolui <107446003+ArpanDolui@users.noreply.github.com>
2025-02-18 10:14:17 +0530

    Update 75_Sort_Colors.java

--- 75_Sort_Colors.java
+++ 75_Sort_Colors.java
@@ -22,3 +22,14 @@ class Solution {
         nums[j] = temp;
     }
 }
+
+///////////////////////////////
+// Test case
+Example 1:
+
+Input: nums = [2,0,2,1,1,0]
+Output: [0,0,1,1,2,2]
+Example 2:
+
+Input: nums = [2,0,1]
+Output: [0,1,2]
